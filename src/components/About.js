9bef0c088974e695a464af63c063ffcb562927e9
Maureen <mlincke15@gmail.com>
2022-04-02 18:18:58 -0600

    adding image to about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,22 @@
-import { Heading, VStack } from "@chakra-ui/react";
+import { Container, Flex, Heading, VStack, Text, Box, Image } from "@chakra-ui/react";
 
 const About = () => {
     return (
-        <VStack>
-            <Heading>Maureen Lincke</Heading>
-        </VStack>
+        <Container maxW='container.xl' p={0}>
+            <Flex h='50vh' py={20}>
+                <VStack w='full' h="full" p={10} spacing={10} >
+                    <VStack spacing={3} alignItems='flex-end'>
+                        <Heading size='2xl'>Maureen Lincke</Heading>
+                        <Text>Full Stack Software Engineer</Text>
+                    </VStack>
+                </VStack>
+                <VStack w='full' h="full" p={10} spacing={10} bg='gray.50'>
+                    <Box>
+                        <Image src='https://i.ibb.co/16CvWW6/Capture-2.jpg' />
+                    </Box>
+                </VStack> 
+            </Flex>
+        </Container>
     )
 }
 
