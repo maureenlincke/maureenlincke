b8277e3eae27fffafaf7887d87ef3e2441b3142f
Maureen <mlincke15@gmail.com>
2022-04-14 14:02:08 -0600

    moving about image to left of text

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,20 @@
 import { Container, Flex, Heading, VStack, Text, Box, Image } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 const About = () => {
     return (
         <Container maxW='container.xl' p={0}>
             <Flex h='50vh' py={20}>
+                <Box spacing={10} p={10} >
+                    <Image borderRadius='full' src='https://i.ibb.co/16CvWW6/Capture-2.jpg' />
+                </Box>
                 <VStack w='full' h="full" p={10} spacing={10} >
-                    <VStack spacing={3} alignItems='flex-end'>
+                    <VStack spacing={3} alignItems='flex-start'>
                         <Heading size='2xl'>Maureen Lincke</Heading>
+                        
                         <Text>Full Stack Software Engineer</Text>
                     </VStack>
                 </VStack>
-                <VStack w='full' h="full" p={10} spacing={10} bg='gray.50'>
-                    <Box>
-                        <Image src='https://i.ibb.co/16CvWW6/Capture-2.jpg' />
-                    </Box>
-                </VStack> 
             </Flex>
         </Container>
     )
