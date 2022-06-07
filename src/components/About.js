349e12b95c5d57ed17694921c598f9fb89bcd8f4
Maureen <mlincke15@gmail.com>
2022-06-07 15:36:04 -0600

    adding fonts, messing with container width

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,16 @@ import { motion } from "framer-motion";
 
 const About = () => {
     return (
-        <Container maxW='' >
+        <Container maxW='100vw' >
             <Flex h='100vh' py={20}>
                 <Box spacing={10} p={10} >
                     <Image borderRadius='full' src='https://i.ibb.co/16CvWW6/Capture-2.jpg' />
                 </Box>
                 <VStack w='full' h="full" p={10} spacing={10} >
                     <VStack spacing={3} alignItems='flex-start'>
-                        <Heading size='2xl'>Maureen Lincke</Heading>
+                        <Heading size='2xl' fontFamily='Roboto' fontWeight='400'>Maureen Lincke</Heading>
                         
-                        <Text>Full Stack Software Engineer</Text>
+                        <Text fontFamily='Roboto' fontWeight='300'>Full Stack Software Engineer</Text>
                     </VStack>
                 </VStack>
             </Flex>
